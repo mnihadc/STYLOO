fb03d37125a4ec1087209a9bd8f25a54e664d3d3
Nihad <mncnihad@gmail.com>
2025-05-07 11:33:47 +0530

    updated the app.js

--- Backend/App.js
+++ Backend/App.js
@@ -1,13 +1,19 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
-
+const __dirname = path.resolve();
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "/Client/dist")));
+
+
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is Running on port ${port}!!!`);
 });
